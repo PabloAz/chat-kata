cbd9400ff75fc58d874ef614c0c769603aad0c69
pablo.azana <pablo.azana@bqreaders.com>
2013-06-24 14:03:26 +0200

    Terminada la parte 4 y 5 del tutorial

--- server/grails-app/controllers/chat/kata/ChatController.groovy
+++ server/grails-app/controllers/chat/kata/ChatController.groovy
@@ -8,23 +8,34 @@ class ChatController {
 	ChatService chatService
 
 	def list(Integer seq) {
+		
 		if(hasErrors()){
+			
 			log.error("Invalid seq: ${errors.getFieldError('seq').rejectedValue}")
         	//TODO: send error about invalid seq
-		}
-		
-		Collection<ChatMessage> newMessages = new ArrayList()
-		Integer last_seq2 = chatService.collectChatMessages(newMessages, seq)
-		
-		
-		render(contentType: "text/json"){
-			messages = []
-				
-			for(m in newMessages){
-				messages.add(nick:m.getNick(),message:m.getMessage())
+			
+			render(status:400,contentType: "text/json") {
+				error = "Invalid seq parameter"
 			}
 			
-			last_seq = last_seq2	
+		}
+		
+		else{
+		
+			Collection<ChatMessage> newMessages = new ArrayList()
+			Integer last_seq2 = chatService.collectChatMessages(newMessages, seq)
+			
+			
+			render(contentType: "text/json"){
+				messages = []
+					
+				for(m in newMessages){
+					messages.add(nick:m.getNick(),message:m.getMessage())
+				}
+				
+				last_seq = last_seq2	
+				
+			}
 			
 		}
 		
@@ -32,9 +43,38 @@ class ChatController {
 
 	def send(){
 		
-		chatService.putChatMessage(new ChatMessage(request.JSON))
-		render(status:201)
+		if(!request.JSON){
+			
+			render(status:400,contentType: "text/json") {
+				error = "Invalid body"
+			}
+		}
 		
+		else{
+			
+			ChatMessage chatMsg = new ChatMessage(request.JSON)
+			
+			if(!chatMsg.validate()){
+				
+				if (chatMsg.errors.hasFieldErrors("nick")) {
+					render(status:400,contentType: "text/json") {
+						error = "Missing nick parameter"
+					}
+				}
+				
+				if (chatMsg.errors.hasFieldErrors("message")) {
+					render(status:400,contentType: "text/json") {
+						error = "Missing message parameter"
+					}
+				}
+				
+			}
+			
+			else{
+				chatService.putChatMessage(new ChatMessage(request.JSON))
+				render(status:201)
+			}
+		}
 		
 	}
 }
