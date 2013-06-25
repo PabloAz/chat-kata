a101cd269a8a448981c37b57ce91653c9a6ffffb
pablo.azana <pablo.azana@bqreaders.com>
2013-06-25 12:33:45 +0200

    Creadas las interfaces gráficas para el login y la pantalla de chat

--- client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
+++ client/SimpleChat/src/org/ejmc/android/simplechat/LoginActivity.java
@@ -1,8 +1,14 @@
 package org.ejmc.android.simplechat;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * Main activity.
@@ -13,11 +19,35 @@ import android.view.Menu;
  * 
  */
 public class LoginActivity extends Activity {
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+		
+		Button btnLogin =(Button) findViewById(R.id.button1);
+		final EditText name = (EditText) findViewById(R.id.editText1);
+		
+		btnLogin.setOnClickListener(new Button.OnClickListener() {  
+	        public void onClick(View v){
+	        	/* Almacenamos el nombre del usuario */
+	        	
+	        	String nameText = name.getText().toString();
+	        	
+	        	SharedPreferences settings = getSharedPreferences("perfil", MODE_PRIVATE);
+	        	SharedPreferences.Editor editor = settings.edit();
+	        	editor.putString("user",nameText);
+	        	editor.commit();
+	        	
+	        	Intent inten = new Intent(LoginActivity.this, ChatActivity.class);
+	        	startActivity(inten);
+	        	
+	        	//Toast notificacionToast=Toast.makeText(getApplicationContext(),settings.getString("user", "No existe"),Toast.LENGTH_SHORT);
+	    		//snotificacionToast.show();
+	        }
+	    });
+			
 	}
 
 	@Override
@@ -26,5 +56,5 @@ public class LoginActivity extends Activity {
 		getMenuInflater().inflate(R.menu.login, menu);
 		return true;
 	}
-
+		
 }
